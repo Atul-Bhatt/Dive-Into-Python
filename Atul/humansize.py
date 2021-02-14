f4b8c6867c8faa70519c7215c053c05f5a67f6fd
I516396 <atul.bhatt@sap.com>
2021-02-14 15:19:37 +0530

    Docstring added in humansize

--- Atul/humansize.py
+++ Atul/humansize.py
@@ -1,7 +1,13 @@
 SUFFIXES = {1000: ['KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'],
             1024: ['KiB', 'MiB', 'GiB', 'TiB', 'PiB', 'EiB', 'ZiB', 'YiB']}
 
+
 def approximate_size(size, a_kilobyte_is_1024_bytes = True):
+    '''
+    Convert a file size to human readable form
+    size -- size of file in bytes
+    a_kilobyte_is_1024_bytes -- if true, use 1024, else use 1000
+    '''
     if size < 0:
         raise ValueError('Number must be non-negative.')
 
@@ -13,6 +19,7 @@ def approximate_size(size, a_kilobyte_is_1024_bytes = True):
             return '{0:.1f} {1}'.format(size, suffix)
 
     raise ValueError('number too large.')
+
 
 if __name__ == '__main__':
     print(approximate_size(100000000000, False))
